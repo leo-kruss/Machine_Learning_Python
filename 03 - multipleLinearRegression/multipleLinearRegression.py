0876ccd170d8df5c9cbb07bff0825aa99f80eb77
Leonardo <leo.kruss@gmail.com>
2022-08-01 16:49:52 -0300

    Multiple Linear Regression

--- 03 - multipleLinearRegression/multipleLinearRegression.py
+++ 03 - multipleLinearRegression/multipleLinearRegression.py
@@ -23,5 +23,9 @@ x_train, x_test, y_train, y_test, = train_test_split(x, y, test_size = 0.2, rand
 #training the multiple linear regression model on the training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
+regressor.fit(x_train, y_train)
 
 #predicting the test set results
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision = 2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
